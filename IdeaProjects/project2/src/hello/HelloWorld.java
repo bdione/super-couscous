f0dedbfe0884046778064173da17c2be055bd546
bdione <baba934@gmail.com>
2019-09-03 23:00:10 -0400

    my first pushing

---
+++ IdeaProjects/project2/src/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class HelloWorld {
+
+    public static void main(String[] args);
+
+           }int address = 31;
+          system.out.println ("hello world")
+                  }
+                  }
